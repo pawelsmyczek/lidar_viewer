0c965c00d386a3abbec24e77ed27284ff83ac77a
Marcin Kruk <marcin.kruk@globallogic.com>
2024-10-31 12:01:14 +0100

    Implement StatusOr and setup enviroment for UnitTests

--- ui/inc/Viewer.h
+++ ui/inc/Viewer.h
@@ -2,71 +2,65 @@
 #define LIDAR_VIEWER_VIEWER_H
 
 #include "CygLidarD1.h"
-#include <vector>
 #include <functional>
+#include <vector>
 
-namespace lidar_viewer::ui
-{
+namespace lidar_viewer::ui {
 
 ///@brief function to be viewed by viewer
-struct ViewerFunction
-{
-    void(*function)(const void*);
-    const void* worker;
+struct ViewerFunction {
+  void (*function)(const void *);
+  const void *worker;
 };
 
-/// @brief viewer, performs display actions using registered objects via registerViewerFunction
-class Viewer
-{
+/// @brief viewer, performs display actions using registered objects via
+/// registerViewerFunction
+class Viewer {
 public:
-    enum class ViewType
-    {
-        Flat, PointCloud
-    };
-    struct Config
-    {
-        unsigned int x;
-        unsigned int y;
-        unsigned int w;
-        unsigned int h;
-        ViewType viewType;
-    };
+  enum class ViewType { Flat, PointCloud };
+  struct Config {
+    unsigned int x;
+    unsigned int y;
+    unsigned int w;
+    unsigned int h;
+    ViewType viewType;
+  };
 
-    Viewer(Config configuration, int* argc, char** argv) noexcept;
+  Viewer(Config configuration, int *argc, char **argv) noexcept;
 
-    /// starts displaying using Config passed as a ctor, shall be used as a main thread loop
-    void start() const;
+  /// starts displaying using Config passed as a ctor, shall be used as a main
+  /// thread loop
+  void start() const;
 
-    void display();
+  void display();
 
-    void rotateUp() noexcept;
-    void rotateDown() noexcept;
-    void rotateLeft() noexcept;
-    void rotateRight() noexcept;
-    void scaleUp() noexcept;
-    void scaleDown() noexcept;
-    void zeroWindowTransforms() noexcept;
+  void rotateUp() noexcept;
+  void rotateDown() noexcept;
+  void rotateLeft() noexcept;
+  void rotateRight() noexcept;
+  void scaleUp() noexcept;
+  void scaleDown() noexcept;
+  void zeroWindowTransforms() noexcept;
 
-    /// @brief registers a function to draw a particular object
-    /// @param func object drawing function
-    /// @param obj object to work on
-    template < class Object >
-    void registerViewerFunction( void(*func)(const Object*), const Object* obj)
-    {
-        viewerFuncitons.push_back(
-                ViewerFunction{ .function = reinterpret_cast<void (*)(const void*)>(func),
-                                .worker = obj }
-                 );
-    }
+  /// @brief registers a function to draw a particular object
+  /// @param func object drawing function
+  /// @param obj object to work on
+  template <class Object>
+  void registerViewerFunction(void (*func)(const Object *), const Object *obj) {
+    viewerFuncitons.push_back(ViewerFunction{
+        .function = reinterpret_cast<void (*)(const void *)>(func),
+        .worker = obj});
+  }
+
 private:
-    std::vector<ViewerFunction> viewerFuncitons;
-    Config configuration;
-    std::atomic<int> rotx;
-    std::atomic<int> roty;
-    float windowScale;
-    mutable std::mutex mutex;
+  std::vector<ViewerFunction> viewerFuncitons;
+  Config configuration;
+  std::atomic<int> rotx;
+  std::atomic<int> roty;
+  float windowScale;
+  mutable std::mutex mutex;
 };
 
-}
+} // namespace lidar_viewer::ui
 
-#endif //LIDAR_VIEWER_VIEWER_H
+#endif // LIDAR_VIEWER_VIEWER_H
